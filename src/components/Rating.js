dddc1e1ac9321883980bfe30f7e001ef7439ad2d
Srishti918 <srishtimalaviya123@gmail.com>
2023-11-06 21:29:15 +0530

    updated star component

---
+++ src/components/Rating.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react'
+import {AiOutlineStar} from "react-icons/ai"
+import "./Rating.css"
+const Rating = () => {
+    const [rating,setRating]=useState(0);
+
+  return (
+    <div className='star-rating'>
+        {[...Array(5)].map((star,i)=>{
+            i+=1;
+            return(
+                <button
+                    type="button"
+                    key={i}
+                    className={i<=rating? "on": "off"}
+                    onClick={()=> setRating(i)}
+                    >
+                        <span className="star"><AiOutlineStar /></span>
+                    </button>
+            )
+        })}
+      
+    </div>
+  )
+}
+
+export default Rating
+
