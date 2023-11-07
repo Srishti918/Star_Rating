1c1c6337a3e671deb29fed6020e64b3f88c097e5
Srishti918 <srishtimalaviya123@gmail.com>
2023-11-07 13:18:17 +0530

    Added hover functionality

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,6 +3,7 @@ import {AiOutlineStar} from "react-icons/ai"
 import "./Rating.css"
 const Rating = () => {
     const [rating,setRating]=useState(0);
+    const [hover,setHover]=useState(0);
 
   return (
     <div className='star-rating'>
@@ -12,8 +13,10 @@ const Rating = () => {
                 <button
                     type="button"
                     key={i}
-                    className={i<=rating? "on": "off"}
+                    className={i<=(rating || hover)? "on": "off"}
                     onClick={()=> setRating(i)}
+                    onMouseEnter={()=>setHover(i)}
+                    onMouseLeave={()=>{setHover(rating)}}
                     >
                         <span className="star"><AiOutlineStar /></span>
                     </button>
